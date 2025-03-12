79a52d51eb02d1ad3613c482a7ce1d752a75b9e3
fatimaalvarez-creator <A01645815@tec.mx>
2025-03-12 12:15:49 -0600

    Update frequency_meter.v

--- Challenge_1_Frecuenciometro/frequency_meter.v
+++ Challenge_1_Frecuenciometro/frequency_meter.v
@@ -1,5 +1,4 @@
-// Mide la frecuencia de una señal y la muestra en un display de 7 segmentos
-
+// mide la frecuencia de una señal y la muestra en un display de 7 segmentos
 module frequency_meter #(parameter INVERT_RST = 1, DEBOUNCE_THRESHOLD = 5000, SEGMENTOS = 7, BIT_SIZE = 20, FREQ = 1, DATA_IN_SIZE = 6)(
     input clk,
     input rst,
@@ -7,15 +6,17 @@ module frequency_meter #(parameter INVERT_RST = 1, DEBOUNCE_THRESHOLD = 5000, SE
     output [0:SEGMENTOS-1] D_decenas, D_unidades, D_centenas, D_millares, D_decenas_millares, D_centenas_millares
 );
 
+// auxiliares    
 wire one_shot_rst;
 
+// instanciamos el debouncer_one_shot    
 debouncer_one_shot #(.INVERT_LOGIC(INVERT_RST), .DEBOUNCE_THRESHOLD(DEBOUNCE_THRESHOLD)) DEB_ONE_SHOT_RST (
     .clk(clk),
     .signal(rst),
     .signal_one_shot(one_shot_rst)
 );
 
-// Sincronización de la señal de entrada
+// instanciamos sincronización de la señal de entrada
 wire synced_signal;
 synchronizer SYNCHRONIZER (
     .clk(clk),
@@ -23,7 +24,7 @@ synchronizer SYNCHRONIZER (
     .signal_sync(synced_signal)
 );
 
-// Detección de flanco ascendente
+// instancciamos la detección de flanco ascendente
 wire rise_edge;
 edge_detector EDGE_DETECTOR (
     .clk(clk),
@@ -31,7 +32,7 @@ edge_detector EDGE_DETECTOR (
     .rise_edge(rise_edge)
 );
 
-// Captura del período
+// instanciamos período
 wire [23:0] period;
 period_capture PERIOD_CAPTURE (
     .clk(clk),
@@ -40,7 +41,7 @@ period_capture PERIOD_CAPTURE (
     .period(period)
 );
 
-// Cálculo de frecuencia (50 MHz / período)
+// instanciamos el cálculo de frecuencia (50 MHz / período)
 wire [BIT_SIZE-1:0] frequency;
 frequency_calculator FREQUENCY_CALCULATOR (
     .clk(clk),
@@ -49,6 +50,7 @@ frequency_calculator FREQUENCY_CALCULATOR (
     .frequency(frequency)
 );
 
+// instanciamos los displays    
 display_module #(.BIT_SIZE(BIT_SIZE)) DISPLAY_MODULE (
     .number(frequency),
     .D_unidades(D_unidades),
